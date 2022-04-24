12744c5130c9a2b415b666acc84fd3be1a83c540
Shiv Rana <shivrana527@gmail.com>
2022-04-24 11:08:53 -0400

    Addition functionality added

--- src/main/java/com/shiv/calculator/App.java
+++ src/main/java/com/shiv/calculator/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "This is the Calculator" );
+    }
+    
+    public int addition(int a, int b) {
+    	return a+b;
     }
 }
